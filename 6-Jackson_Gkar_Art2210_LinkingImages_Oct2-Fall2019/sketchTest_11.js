417c799c56ec3d044788e4bf493d807449e71450
Gmanski504 <55206352+Gmanski504@users.noreply.github.com>
2019-10-02 14:51:27 -0500

    Add files via upload

--- 6-Jackson_Gkar_Art2210_LinkingImages_Oct2-Fall2019/sketchTest_11.js
+++ 6-Jackson_Gkar_Art2210_LinkingImages_Oct2-Fall2019/sketchTest_11.js
@@ -1,7 +1,3 @@
-function preload(){
-    img=loadImage("https://github.com/Gmanski504/Jackson_Gkar_ART2210/blob/master/3-Jackson_Gkar_ART2210_Self-portrait_Sep23_Fall2019/Img/vasilykandinsky.jpg");
-
-
 function setup() {
   createCanvas(windowWidth,windowHeight);
 }
@@ -10,26 +6,7 @@ function draw() {
   background(25);
   
 
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
  function windowResized(){
